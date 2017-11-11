06f3c4dd907a1124dc47d151f2268890e1f5ca6a
Sarah GP <sarah.groff.palermo@gmail.com>
2017-11-11 18:09:29 -0500

    Updates with interval

--- ui_src/ui/core.cljs
+++ ui_src/ui/core.cljs
@@ -6,34 +6,19 @@
 
 (println "Loaded.")
 
+;; settings
 (def width (atom (.-innerWidth js/window)))
 (def height (atom (.-innerHeight js/window)))
 
 (def settings {:width @width
-               :height @height
-               :variation 12
-               :stroke "#ff7d66"
-               :stroke-width 2
-               })
+               :height @height })
 
-(def test-fill-id "rect-circles-2")
 
-(defn gen-test-params
-  [fill-id transform]
-  { :class ""
-    :style { :fill (str "url(#" fill-id ") #fff")
-             :transform transform}
-    :d "M103.5 0 186.407601 37.9720691 206.884087 123.294534 149.510247 191.717838 57.4897527 191.717838 0.115912866 123.294534 20.5923992 37.9720691z"
-    :id fill-id
-    :image-link "data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+CiAgPHJlY3Qgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJyBmaWxsPSdoc2xhKDM2MCwgMTAwJSwgMTAwJSwgMCknIC8+CiAgPGNpcmNsZSBjeD0nMS41JyBjeT0nMS41JyByPScxLjUnIGZpbGw9JyNmZjAwNDgnLz4KPC9zdmc+Cg==" })
+;; shape primitives
+(def hept "M103.5 0 186.407601 37.9720691 206.884087 123.294534 149.510247 191.717838 57.4897527 191.717838 0.115912866 123.294534 20.5923992 37.9720691z")
 
-(def test-params-01 (gen-test-params test-fill-id "translate(100px, 100px)"))
-(def test-params-02 (gen-test-params test-fill-id "translate(0px, 0px)"))
-
-(defn shape
-  [{ :keys [class d style] }]
-  [:path {:class class :d d :style style}])
-
+;; fill primitives
+;; patterns
 (defn pattern
   [{ :keys [id image-link] }]
   [:pattern { :id id
@@ -46,19 +31,62 @@
               :width "10"
               :height "10" }]])
 
-(def show? (atom false))
+(def pink-squares "rect-circles-2")
 
-(defonce show-updater
+;; solid colors
+
+(defn gen-test-params
+  [fill-id transform]
+  { :class ""
+    :style { :fill (str "url(#" fill-id ") #fff")
+             :transform transform}
+    :d hept
+    :id fill-id
+    :item-key (random-uuid)
+    ;; break this into pattern
+    :image-link "data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+CiAgPHJlY3Qgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJyBmaWxsPSdoc2xhKDM2MCwgMTAwJSwgMTAwJSwgMCknIC8+CiAgPGNpcmNsZSBjeD0nMS41JyBjeT0nMS41JyByPScxLjUnIGZpbGw9JyNmZjAwNDgnLz4KPC9zdmc+Cg=="
+    })
+
+(def test-params-01 (gen-test-params pink-squares "translate(100px, 100px)"))
+(def test-params-02 (gen-test-params pink-squares "translate(0px, 0px)"))
+
+(defn shape
+  [{ :keys [class d style item-key] }]
+  [:path { :key item-key :class class :d d :style style }])
+
+(defn even-frame [frame] (= 0 (mod frame 2)))
+
+
+;; ----------- COLLECTION SETUP AND CHANGE ----------------
+
+(defonce collection (atom (list)))
+
+(defn cx [frame] (list
+  (when (even-frame frame) (shape test-params-02))
+  (shape test-params-01)
+  ))
+
+
+;; ----------- LOOP AND DRAW ------------------------------
+
+
+(defonce frame (atom 0))
+
+(defn frame-and-draw []
+  (swap! frame inc)
+  (reset! collection (cx @frame)))
+
+(defonce start-timer
   (js/setInterval
-    #(swap! show? not)
-    500))
+    #(frame-and-draw) 500))
 
 (defn drawing []
-  ;; add event listener for when width changes
   [:svg { :height (:width settings), :width (:height settings) }
+    ;; eventually this should take in all the patterns
     [:defs (pattern test-params-01)]
-    (shape test-params-01)
-    (when (= true @show?) (shape test-params-02))])
+
+    ;; then here dereference a state full of shapes
+    @collection ])
 
 (reagent/render-component [drawing]
                           (js/document.getElementById "app-container"))
